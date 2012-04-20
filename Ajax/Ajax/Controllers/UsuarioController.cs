311d94367930a7a1b423e337155984613f7df340
Breno Ferreira <bferreira@windowslive.com>
2012-04-19 22:29:50 -0300

    Adicionado projeto Ajax

---
+++ Ajax/Ajax/Controllers/UsuarioController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ajax.Models;
+
+namespace Ajax.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private List<Usuario> usuarios;
+        public UsuarioController()
+        {
+            this.usuarios = new List<Usuario>();
+            Usuario u1 = new Usuario();
+            u1.UserName = "Joao";
+            u1.Senha = "123";
+            this.usuarios.Add(u1);
+
+            Usuario u2 = new Usuario();
+            u2.UserName = "Maria";
+            u2.Senha = "1234";
+            this.usuarios.Add(u2);
+        }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public String Cadastro(Usuario usuario)
+        {
+            this.usuarios.Add(usuario);
+            return "OK";
+        }
+
+        //Get: /Usuario/VerificaUsuario?username=joao
+        public Boolean VerificaUsuario(String username)
+        {
+            Boolean achou = false;
+            foreach (Usuario usuario in this.usuarios)
+            {
+                if (usuario.UserName == username)
+                {
+                    achou = true;
+                    break;
+                }
+            }
+
+            return achou;
+        }
+    }
+}
---
+++ Ajax/Ajax/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ajax.Models
+{
+    public class Usuario
+    {
+        private string username;
+
+        public string UserName
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+
+        private String senha;
+
+        public String Senha
+        {
+            get { return senha; }
+            set { senha = value; }
+        }
+
+    }
+}
